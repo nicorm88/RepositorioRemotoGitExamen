71b2af38bb34b3b7d72839558403187eba90075e
DAM1 <DAM1@DESKTOP-3LHBG1B>
2023-05-11 20:06:13 +0200

    nuevoscambios

---
+++ ProyectoGitExamen/src/paqueteGitExamen/NuevoMain.java
@@ -0,0 +1,11 @@
+package paqueteGitExamen;
+
+public class NuevoMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Palabra1");
+		System.out.println("Palabra2");
+	}
+
+}
